dfc3e239d0c472968bb04ed1ce489b62a433ae52
sanmankrish <46064644+sanmankrish@users.noreply.github.com>
2018-12-22 13:53:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 <h1>This is my PHP Test Application<h1>
   <p> This is my aws bean stalk paas</p>
   <p1> hey boss code is ready</p1>
-  <p> shankar direction new movie is idian 2  </p>
-  <p> Mass prince  is robo 3 and microbots  and rajini is back  amy robot is good </p>
+  <p> Swami Vivekananda is the Youth Icon of INDIA  </p>
+  <p> His Messages are UNIVERSAL </p>
 
 <?php
 echo "Version: 5.2";
